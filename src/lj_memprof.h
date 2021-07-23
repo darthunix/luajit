59867e73c91c352d8ee409feed39854e318a0f61
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-07-23 20:44:26 +0300

    memprof: move symtab to a separate module
    Considering the symbol table format, it is obvious that it is suitable
    not only for memprof, but for other profiling modules too. This commit
    moves the symbol table to the separate module, so modules other than
    memprof will be able to access it.
    
    Part of tarantool/tarantool#781

--- src/lj_memprof.h
+++ src/lj_memprof.h
@@ -16,42 +16,6 @@
 #include "lj_def.h"
 #include "lj_wbuf.h"
 
-#define LJS_CURRENT_VERSION 0x1
-
-/*
-** symtab format:
-**
-** symtab         := prologue sym*
-** prologue       := 'l' 'j' 's' version reserved
-** version        := <BYTE>
-** reserved       := <BYTE> <BYTE> <BYTE>
-** sym            := sym-lua | sym-final
-** sym-lua        := sym-header sym-addr sym-chunk sym-line
-** sym-header     := <BYTE>
-** sym-addr       := <ULEB128>
-** sym-chunk      := string
-** sym-line       := <ULEB128>
-** sym-final      := sym-header
-** string         := string-len string-payload
-** string-len     := <ULEB128>
-** string-payload := <BYTE> {string-len}
-**
-** <BYTE>   :  A single byte (no surprises here)
-** <ULEB128>:  Unsigned integer represented in ULEB128 encoding
-**
-** (Order of bits below is hi -> lo)
-**
-** version: [VVVVVVVV]
-**  * VVVVVVVV: Byte interpreted as a plain numeric version number
-**
-** sym-header: [FUUUUUTT]
-**  * TT    : 2 bits for representing symbol type
-**  * UUUUU : 5 unused bits
-**  * F     : 1 bit marking the end of the symtab (final symbol)
-*/
-
-#define SYMTAB_LFUNC ((uint8_t)0)
-#define SYMTAB_FINAL ((uint8_t)0x80)
 
 #define LJM_CURRENT_FORMAT_VERSION 0x01
 
