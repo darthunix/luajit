13a5af2d5dca208448cebb221288525486546999
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-07-23 20:44:26 +0300

    memprof: move symtab to a separate module
    Considering the symbol table format, it is obvious that it is suitable
    not only for memprof, but for other profiling modules too. This commit
    moves the symbol table to the separate module, so modules other than
    memprof will be able to access it.
    
    Part of tarantool/tarantool#781

--- src/lj_memprof.c
+++ src/lj_memprof.c
@@ -19,86 +19,8 @@
 #include "lj_frame.h"
 #include "lj_debug.h"
 
-#if LJ_HASJIT
-#include "lj_dispatch.h"
-#endif
-
-/* --------------------------------- Symtab --------------------------------- */
-
-static const unsigned char ljs_header[] = {'l', 'j', 's', LJS_CURRENT_VERSION,
-					   0x0, 0x0, 0x0};
-
-#if LJ_HASJIT
-
-static void dump_symtab_trace(struct lj_wbuf *out, const GCtrace *trace)
-{
-  GCproto *pt = &gcref(trace->startpt)->pt;
-  BCLine lineno = 0;
-
-  const BCIns *startpc = mref(trace->startpc, const BCIns);
-  lua_assert(startpc >= proto_bc(pt) &&
-             startpc < proto_bc(pt) + pt->sizebc);
-
-  lineno = lj_debug_line(pt, proto_bcpos(pt, startpc));
-
-  lj_wbuf_addbyte(out, SYMTAB_TRACE);
-  lj_wbuf_addu64(out, (uint64_t)trace->traceno);
-  lj_wbuf_addu64(out, (uint64_t)trace->mcode);
-  /*
-  ** The information about the prototype, associated with the
-  ** trace's start has already been dumped, as it is anchored
-  ** via the trace and is not collected while the trace is alive.
-  ** For this reason, we do not need to repeat dumping the chunk
-  ** name for the prototype.
-  */
-  lj_wbuf_addu64(out, (uintptr_t)pt);
-  lj_wbuf_addu64(out, (uint64_t)lineno);
-}
-
-#else
-
-static void dump_symtab_trace(struct lj_wbuf *out, const GCtrace *trace)
-{
-  UNUSED(out);
-  UNUSED(trace);
-  lua_assert(0);
-}
-
-#endif
-
-static void dump_symtab(struct lj_wbuf *out, const struct global_State *g)
-{
-  const GCRef *iter = &g->gc.root;
-  const GCobj *o;
-  const size_t ljs_header_len = sizeof(ljs_header) / sizeof(ljs_header[0]);
-
-  /* Write prologue. */
-  lj_wbuf_addn(out, ljs_header, ljs_header_len);
-
-  while ((o = gcref(*iter)) != NULL) {
-    switch (o->gch.gct) {
-    case (~LJ_TPROTO): {
-      const GCproto *pt = gco2pt(o);
-      lj_wbuf_addbyte(out, SYMTAB_LFUNC);
-      lj_wbuf_addu64(out, (uintptr_t)pt);
-      lj_wbuf_addstring(out, proto_chunknamestr(pt));
-      lj_wbuf_addu64(out, (uint64_t)pt->firstline);
-      break;
-    }
-    case (~LJ_TTRACE): {
-      dump_symtab_trace(out, gco2trace(o));
-      break;
-    }
-    default:
-      break;
-    }
-    iter = &o->gch.nextgc;
-  }
-
-  lj_wbuf_addbyte(out, SYMTAB_FINAL);
-}
-
 /* ---------------------------- Memory profiler ----------------------------- */
+#include "lj_symtab.h"
 
 enum memprof_state {
   /* Memory profiler is not running. */
@@ -331,7 +253,7 @@ int lj_memprof_start(struct lua_State *L, const struct lj_memprof_options *opt)
 
   /* Init output. */
   lj_wbuf_init(&mp->out, mp_opt->writer, mp_opt->ctx, mp_opt->buf, mp_opt->len);
-  dump_symtab(&mp->out, mp->g);
+  lj_symtab_dump(&mp->out, mp->g);
 
   /* Write prologue. */
   lj_wbuf_addn(&mp->out, ljm_header, ljm_header_len);
